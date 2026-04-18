3604b39207cee2bf9bc1ab09d1232745e4d63047
nazarSHP99 <145098692+nazarSHP99@users.noreply.github.com>
2026-04-18 09:17:34 +0300

    refactor: remove auth and profile testing as requested

--- Lab9.E2E/pages/page-factory.js
+++ Lab9.E2E/pages/page-factory.js
@@ -1,6 +1,4 @@
 import { HomePage }    from './home-page.js';
-import { SignInPage }  from './sign-in-page.js';
-import { ProfilePage } from './profile-page.js';
 
 export class PageFactory {
   /** @param {import('@playwright/test').Page} page */
@@ -8,7 +6,5 @@ export class PageFactory {
     this.page = page;
   }
 
-  home()           { return new HomePage(this.page); }
-  signIn()         { return new SignInPage(this.page); }
-  profile(email)   { return new ProfilePage(this.page, email); }
+  home() { return new HomePage(this.page); }
 }
